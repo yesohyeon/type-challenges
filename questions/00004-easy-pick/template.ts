87b746a937df388b6629c7cc27ddccde0f4fc485
Sohyeon <needtlc13@gmail.com>
2023-05-14 21:57:15 +0900

    pick4 문제 풀이

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,34 @@
-type MyPick<T, K> = any;
+type MyPick<T, K extends keyof T> = { [P in K]: T[P] }
+
+// keyof
+// and Lookup Types
+
+interface Person {
+  name: string;
+  age: number;
+  location: string;
+}
+type K1 = keyof Person; // "name" | "age" | "location"
+type K2 = keyof Person[]; // "length" | "push" | "pop" | "concat" | ...
+type K3 = keyof { [x: string]: Person }; // string
+
+type P1 = Person["name"]; // string
+type P2 = Person["name" | "age"]; // string | number
+type P3 = string["charAt"]; // (pos: number) => string
+type P4 = string[]["push"]; // (...items: string[]) => number
+type P5 = string[][0]; // string
+type P6 = string[][0]
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key]; // Inferred type is T[K]
+}
+function setProperty<T, K extends keyof T>(obj: T, key: K, value: T[K]) {
+  obj[key] = value;
+}
+
+let x = { foo: 10, bar: "hello!" };
+let foo = getProperty(x, "foo"); // number
+let bar = getProperty(x, "bar"); // string
+let oops = getProperty(x, "wargarbl"); // Error! "wargarbl" is not "foo" | "bar"
+setProperty(x, "foo", "string"); // Error!, string expected number
+
