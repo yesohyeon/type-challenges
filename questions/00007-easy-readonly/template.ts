bb69ea4eef381a6c0342cfbda867c14c86c0f01e
Sohyeon <needtlc13@gmail.com>
2023-05-14 22:05:11 +0900

    readonly 문제 풀이

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,11 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = { readonly [P in keyof T] : T[P] };
+
+//
+interface Card {
+  name: string;
+  price: number
+}
+
+type readOnlyCard = Readonly<Card>;
+let readonlyUser: readOnlyCard = {name: 'Sonata', price: 10000}
+readonlyUser.price = 3 // 에러발생
