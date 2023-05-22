ac32eb749d890abe76f0928faf1d763eb7298189
Sohyeon <needtlc13@gmail.com>
2023-05-22 20:08:28 +0900

    tuple to object 문제 풀이

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,1 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly any[]> = {[key in T[number]]: key}
--- questions/00011-easy-tuple-to-object/test-cases.ts
+++ questions/00011-easy-tuple-to-object/test-cases.ts
@@ -12,3 +12,25 @@ type cases = [
 
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>
+
+// https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+// example of indexing with an arbitrary type is using number to get the type of an array’s elements. We can combine this with typeof to conveniently capture the element type of an array literal:
+const MyArray = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+  { name: "Eve", age: 38 },
+];
+
+type Person = typeof MyArray[number];
+
+type Age = typeof MyArray[number]["age"];
+// Or
+type Age2 = Person["age"];
+
+
+// 다른 정답
+// type TupleToObject<T extends ReadonlyArray<string | number>> = {
+//   [k in T[number]]: k
+// }
+
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.ht
