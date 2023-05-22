b8e40d4bcc29c2aca4c08ddf205935c521d8faa5
Sohyeon <needtlc13@gmail.com>
2023-05-22 20:27:09 +0900

    First 문제 풀이

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,9 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [] ? never : T[0];
+
+// extends
+// https://www.typescriptlang.org/ko/docs/handbook/2/conditional-types.html
+
+// 다른 풀이
+// type First<T extends any[]> = T extends [infer P, ...any[]] ? P : never
+// infer keyword설명 블로그 : https://blog.logrocket.com/understanding-infer-typescript/
+// https://velog.io/@from_numpy/TypeScript-infer
