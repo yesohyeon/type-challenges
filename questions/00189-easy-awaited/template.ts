c5a8b44b17cfa4501303f6a8af00345b87fab0be
Sohyeon <needtlc13@gmail.com>
2023-05-29 21:38:47 +0900

    Awaited 풀이

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,11 @@
-type MyAwaited<T> = any
+type MyAwaited<T> = T extends PromiseLike<infer U> ? MyAwaited<U> : T;
+
+// interface PromiseLike<T> {
+//   /**
+//    * Attaches callbacks for the resolution and/or rejection of the Promise.
+//    * @param onfulfilled The callback to execute when the Promise is resolved.
+//    * @param onrejected The callback to execute when the Promise is rejected.
+//    * @returns A Promise for the completion of which ever callback is executed.
+//    */
+//   then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2>;
+// }
