efc8a779944ae420e9517694361f2b3cf7c0e163
Sohyeon <needtlc13@gmail.com>
2023-06-04 21:24:10 +0900

    Includes 풀이

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,14 @@
-type Includes<T extends readonly any[], U> = any
+// type Includes<T extends readonly any[], U> = {
+//   [P in T[number]]: true
+// }[U] extends true ? true : false;
+
+type IsEqual<T, U> =
+  (<G>() => G extends T ? 1 : 2) extends
+    (<G>() => G extends U ? 1 : 2)
+    ? true
+    : false;
+
+type Includes<T extends readonly any[], U> =
+  T extends [infer First, ...infer Rest]
+    ? IsEqual<First, U> extends true ? true : Includes<Rest, U>
+    : false;
