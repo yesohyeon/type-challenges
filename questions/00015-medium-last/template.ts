30a6bf2c3b357390839c6fae0e217c88c3a9a34c
Sohyeon <needtlc13@gmail.com>
2023-06-27 22:02:50 +0900

    feat: last of array 풀이

--- questions/00015-medium-last/template.ts
+++ questions/00015-medium-last/template.ts
@@ -1,1 +1,4 @@
-type Last<T extends any[]> = any
+type Last<T extends any[]> = [any, ...T][T["length"]];
+
+// 다른 풀이
+// type Last<T extends any[]> = T extends [...any[], infer last] ? last : never;
