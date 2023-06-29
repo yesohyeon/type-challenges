e23becb9182911337619b91df02492158e496517
Sohyeon <needtlc13@gmail.com>
2023-06-29 19:44:36 +0900

    feat: deep readonly 풀이

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,7 @@
-type DeepReadonly<T> = any
+// type DeepReadonly<T> = keyof T extends never
+//   ? T
+//   : { readonly [k in keyof T]: DeepReadonly<T[k]> };
+
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: keyof T[K] extends never ? T[K] : DeepReadonly<T[K]>
+}
