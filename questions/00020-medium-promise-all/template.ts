f0500927753b9c8b5108d36c39c1ac4e672546de
Sohyeon <needtlc13@gmail.com>
2023-07-10 22:34:24 +0900

    feat: promise.all 풀이

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,7 @@
-declare function PromiseAll(values: any): any
+// 마지막 케이스 통과 안함
+// declare function PromiseAll<T extends any[]>(values: readonly [...T]):
+//   Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>;
+
+declare function PromiseAll<T extends any[]>(values: readonly [...T]): Promise<{
+  [key in keyof T]: Awaited<T[key]>
+}>
