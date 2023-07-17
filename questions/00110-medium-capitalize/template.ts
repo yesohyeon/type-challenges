109933a22444d51df6ed0aace59b7a64ebebd7a5
Sohyeon <needtlc13@gmail.com>
2023-07-17 21:07:47 +0900

    feat: my capitalize 풀이

--- questions/00110-medium-capitalize/template.ts
+++ questions/00110-medium-capitalize/template.ts
@@ -1,1 +1,33 @@
-type MyCapitalize<S extends string> = any
+type MyCapitalize<S extends string> = S extends `${infer x}${infer tail}` ? `${Uppercase<x>}${tail}` : S;
+
+// interface ToUpperCase {
+//   a: "A"
+//   b: "B"
+//   c: "C"
+//   d: "D"
+//   e: "E"
+//   f: "F"
+//   g: "G"
+//   h: "H"
+//   i: "I"
+//   j: "J"
+//   k: "K"
+//   l: "L"
+//   m: "M"
+//   n: "N"
+//   o: "O"
+//   p: "P"
+//   q: "Q"
+//   r: "R"
+//   s: "S"
+//   t: "T"
+//   u: "U"
+//   v: "V"
+//   w: "W"
+//   x: "X"
+//   y: "Y"
+//   z: "Z"
+// }
+//
+// type LowerCase = keyof ToUpperCase
+// type MyCapitalize<S extends string> = S extends `${infer First extends LowerCase}${infer Rest}` ? `${ToUpperCase[First]}${Rest}` : S
