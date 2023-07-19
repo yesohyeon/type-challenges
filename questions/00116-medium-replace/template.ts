f57107c3a2df9d4d8a8d9176ecec1446f758308a
Sohyeon <needtlc13@gmail.com>
2023-07-19 22:37:20 +0900

    feat: replace 풀이

--- questions/00116-medium-replace/template.ts
+++ questions/00116-medium-replace/template.ts
@@ -1,1 +1,7 @@
-type Replace<S extends string, From extends string, To extends string> = any
+type Replace<S extends string, From extends string, To extends string> =
+  From extends ''
+    ? S
+    : S extends `${infer V}${From}${infer R}`
+      ? `${V}${To}${R}`
+      : S
+
