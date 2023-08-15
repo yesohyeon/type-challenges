6984cdd5b4860e9ab10a7f1b157b0fcb0c9648cf
Sohyeon <needtlc13@gmail.com>
2023-08-15 21:41:49 +0900

    merge 풀이

--- questions/00599-medium-merge/template.ts
+++ questions/00599-medium-merge/template.ts
@@ -1,1 +1,8 @@
-type Merge<F, S> = any
+// type Merge<F, S> = Omit<F, keyof S> & S
+type Merge<F, S> = {
+  [K in keyof F | keyof S]: K extends keyof S
+    ? S[K]
+    : K extends keyof F
+      ? F[K]
+      : never;
+};
