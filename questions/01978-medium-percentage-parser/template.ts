bac603d57b018167b4f8e191898d0b27eca03953
Sohyeon <needtlc13@gmail.com>
2023-09-06 20:42:48 +0900

    percentage parser 풀이

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,3 @@
-type PercentageParser<A extends string> = any
+type CheckPrefix<T> = T extends '+' | '-' ? T : never;
+type CheckSuffix<T> =  T extends `${infer P}%` ? [P, '%'] : [T, ''];
+type PercentageParser<A extends string> = A extends `${CheckPrefix<infer L>}${infer R}` ? [L, ...CheckSuffix<R>] : ['', ...CheckSuffix<A>];
