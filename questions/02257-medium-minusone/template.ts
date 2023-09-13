46111c07f293ea4da0df7974d2f465d17f2cdd19
Sohyeon <needtlc13@gmail.com>
2023-09-13 20:40:24 +0900

    minus one 풀이

--- questions/02257-medium-minusone/template.ts
+++ questions/02257-medium-minusone/template.ts
@@ -1,1 +1,6 @@
-type MinusOne<T extends number> = any
+type CustomPop<T extends any[]> = T extends [...infer head, any] ? head : never;
+
+type MinusOne<T extends number, A extends any[] = []> = A['length'] extends T
+  ? CustomPop<A>['length']
+  : MinusOne<T, [...A, 0]>
+
