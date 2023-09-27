20f02ba00e426e58166bb4735797f69e11f3c7ba
Sohyeon <needtlc13@gmail.com>
2023-09-27 13:25:41 +0900

    partial of keys 풀이

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,11 @@
-type PartialByKeys<T, K> = any
+type PartialByKeys<T extends {}, U = keyof T> =
+Omit<Partial<Pick<T, U & keyof T>> & Omit<T, U & keyof T>, never>;
+
+//type IntersectionToObj<T> = {
+//   [K in keyof T]: T[K]
+// }
+// type PartialByKeys<T , K = any> = IntersectionToObj<{
+//   [P in keyof T as P extends K ? P : never]?: T[P]
+// } & {
+//   [P in Exclude<keyof T, K>]: T[P]
+// }>
