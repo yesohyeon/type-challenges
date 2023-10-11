aa3a4bee82dba330d421774649fc449821358e70
Sohyeon <needtlc13@gmail.com>
2023-10-11 21:46:16 +0900

    flatten depth 풀이

--- questions/03243-medium-flattendepth/template.ts
+++ questions/03243-medium-flattendepth/template.ts
@@ -1,1 +1,8 @@
-type FlattenDepth = any
+type FlattenDepth<T extends any[], C extends number = 1, U extends any[] = []> = T extends [infer F,...infer R]?
+  F extends any[]?
+    U['length'] extends C?
+      [F, ...FlattenDepth<R, C, U>]
+      :[...FlattenDepth<F, C, [0,...U]>,...FlattenDepth<R, C, U>]
+    :[F,...FlattenDepth<R, C, U>]
+
+  : T;
