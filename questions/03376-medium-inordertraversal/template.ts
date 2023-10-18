08df7dd58f54bbb79eb503e8e2aa19112b517c66
Sohyeon <needtlc13@gmail.com>
2023-10-18 20:21:28 +0900

    inorder traversal 풀이

--- questions/03376-medium-inordertraversal/template.ts
+++ questions/03376-medium-inordertraversal/template.ts
@@ -1,6 +1,9 @@
 interface TreeNode {
-  val: number
-  left: TreeNode | null
-  right: TreeNode | null
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
 }
-type InorderTraversal<T extends TreeNode | null> = any
+
+type InorderTraversal<T extends TreeNode | null, NT extends TreeNode = NonNullable<T>> = T extends null
+  ? []
+  : [...InorderTraversal<NT['left']>, NT['val'], ...InorderTraversal<NT['right']>]
